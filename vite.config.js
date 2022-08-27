7ef1d8276bdbc9f64170c3864b959d6289b3b35f
David2813 <quinterosotodavidjavier@gmail.com>
2022-08-26 19:40:53 -0500

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://david1328.github.io/gh-react-vite/',
   plugins: [react()]
 })
